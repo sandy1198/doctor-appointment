297b22e75309165599b502b03da4243f35b1818b
santhoshjidp <santhosh.j@idp.com>
2025-10-17 00:08:29 +0530

    updated

---
+++ android/app/src/main/java/doctor/appointment/MainActivity.java
@@ -0,0 +1,5 @@
+package doctor.appointment;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'doctor.appointment',
+  appName: 'doctor-appointment',
+  webDir: 'www'
+};
+
+export default config;
